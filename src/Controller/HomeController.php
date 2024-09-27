93bdf7a52582a3cd36ce7631f3d78ced1039d20d
akishajae <akishajae.angeles@gmail.com>
2024-09-27 16:14:30 +0200

    Create-HomeController

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -6,14 +6,14 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\Routing\Attribute\Route;
 
-class Controller1Controller extends AbstractController
+class HomeController extends AbstractController
 {
-    #[Route('/controller1', name: 'app_controller1')]
+    #[Route('/home', name: 'app_home')]
     public function index(): JsonResponse
     {
         return $this->json([
             'message' => 'Welcome to your new controller!',
-            'path' => 'src/Controller/Controller1Controller.php',
+            'path' => 'src/Controller/HomeController.php',
         ]);
     }
 }
